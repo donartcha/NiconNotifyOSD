c61321a45b3ebd36ca91e246a43d4b6904a2a713
Frederick Adolfo Salazar Sanchez <fredefass01@gmail.com>
2013-06-25 18:29:21 -0500

    Se agrega una nueva constante para defiir el tipo de mensaje a mostrar, la Notify_Confirm
    Se documenta toda la clase y se agrega un metodo toString() que retorna un string con todos los datos del Objeto en cuestion

--- src/nicon/notify/core/NiconEvent.java
+++ src/nicon/notify/core/NiconEvent.java
@@ -18,6 +18,7 @@ public class NiconEvent {
     public final static int NOTIFY_OK=0;
     public final static int NOTIFY_ERROR=1;
     public final static int NOTIFY_WARNING=2;
+    public final static int NOTIFY_CONFIRM=3;
     
     private int codeEvent;
     private String titleEvent;
@@ -86,25 +87,57 @@ public class NiconEvent {
         return tipeMessage;
     }
 
+    /**
+     * Este metodo permite ajustar el tipo de mensaje al que será representado
+     * el objeto NiconEvent.
+     * @param tipeMessage 
+     */
     public void setTipeMessage(int tipeMessage) {
         this.tipeMessage = tipeMessage;
     }
 
+    /**
+     * Este metodo retorna el tipo de mensaje que ha sido usado para mostrar
+     * el objeto NiconEvent.
+     * @return int tipeNotify
+     */
     public int getTipeNotify() {
         return tipeNotify;
     }
 
+    /**
+     * Ajusta el tipo de Notificacion que será usada para mostrar el objeto 
+     * NiconEvent, puede ser DesktopNotify o WindowNotify
+     * @param tipeNotify 
+     */
     public void setTipeNotify(int tipeNotify) {
         this.tipeNotify = tipeNotify;
     }
 
+    /**
+     * Este metodo retorna el estado actual del Objeto NiconEvent que se refiere
+     * a si el Evento ha sido visto, o esta siendo almacenado.
+     * @return in stateEvent
+     */    
     public int getStateEvent() {
         return stateEvent;
     }
 
+    /**
+     * Este metodo permite Ajustar el estado Actual del evento puede ser, visto,
+     * o no visto.
+     * @param stateEvent 
+     */
     public void setStateEvent(int stateEvent) {
         this.stateEvent = stateEvent;
     }  
+
+    @Override
+    public String toString() {
+        return "NiconEvent {" + "codeEvent=" + codeEvent + ", titleEvent=" + titleEvent + ", textEvent=" + textEvent + ", tipeMessage=" + tipeMessage + ", tipeNotify=" + tipeNotify + ", stateEvent=" + stateEvent + '}';
+    }
+    
+    
        
 }
 
