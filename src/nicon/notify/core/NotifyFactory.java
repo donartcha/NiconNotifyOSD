c4532de9618f3c9dd8d948b84e7f7dd95f8bf464
Frederick Adolfo Salazar Sanchez <fredefass01@gmail.com>
2013-06-24 20:07:31 -0500

    Se implementa la ConfirmNotify, esta nueva caracteristicas permitirá que los usuario puedan seleccionar de forma grafica una respuesta simple de si o no a una solicitud de confirmacion por parte de la APP, de igual forma se implementa esta nueva herramienta en NotifyFactory.

--- src/nicon/notify/core/NotifyFactory.java
+++ src/nicon/notify/core/NotifyFactory.java
@@ -7,6 +7,7 @@ package nicon.notify.core;
 import com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import nicon.notify.gui.windowNotify.ConfirmNotify;
 import nicon.notify.gui.windowNotify.InputNotify;
 import nicon.notify.gui.windowNotify.MessageNotify;
 import nicon.notify.gui.windowNotify.WindowNotify;
@@ -21,6 +22,8 @@ public class NotifyFactory {
     private static WindowNotify windowNotify;
     private static MessageNotify messageNotify;
     private static InputNotify inputNotify;
+    private static ConfirmNotify confirmNotify;
+    
     private static String response;
     
     /**
@@ -32,7 +35,6 @@ public class NotifyFactory {
      * @throws UnsupportedLookAndFeelException 
      */
     public static void showWindowNotify(NiconEvent ev) throws UnsupportedLookAndFeelException{
-        UIManager.setLookAndFeel(new NimbusLookAndFeel());
         windowNotify=new WindowNotify(ev);
         windowNotify.setVisible(true);
     }
@@ -46,7 +48,6 @@ public class NotifyFactory {
      * @throws UnsupportedLookAndFeelException 
      */
     public static void showNotifyMessage(NiconEvent ev) throws UnsupportedLookAndFeelException{
-        UIManager.setLookAndFeel(new NimbusLookAndFeel());
         messageNotify=new MessageNotify(ev);
         messageNotify.setVisible(true);
     }
@@ -61,7 +62,6 @@ public class NotifyFactory {
      * @throws UnsupportedLookAndFeelException 
      */
     public static String showInputNotify(NiconEvent ev) throws UnsupportedLookAndFeelException{
-        UIManager.setLookAndFeel(new NimbusLookAndFeel());
         inputNotify=new InputNotify(ev);
         inputNotify.setVisible(true);
             while(inputNotify.isShowing()!=true){
@@ -70,5 +70,29 @@ public class NotifyFactory {
                 break;
             }
         return response;
+    }
+    
+    /**
+     * Este metodo permite crear una nueva notificacion del tipo ConfirmNotify
+     * ofrece la opcion de que el usuario pueda responder a una solicitud de forma
+     * simple a traves de esta interfaz y retorna un valor entero representativo de
+     * esa seleccion, en este caso si el usuario acepta la opcion propuesta
+     * se retornará 0 y en caso contrario de que no acepte la propocicion retornara
+     * como opcion seleccionada 0.
+     * 
+     * @param event
+     * @return int optionSelected
+     */
+    public static int showConfirmNotify(NiconEvent event){
+        int option=-1;
+            confirmNotify=new ConfirmNotify(event);
+            confirmNotify.setVisible(true);
+            
+                while(confirmNotify.isShowing()!=true){
+                    option=confirmNotify.getSelectedOption();
+                    System.out.println("opcion seleccionada: "+option);
+                    break;
+                }
+        return option;
     }
 }
--- src/nicon/notify/gui/windowNotify/ConfirmNotify.java
+++ src/nicon/notify/gui/windowNotify/ConfirmNotify.java
@@ -4,10 +4,70 @@
  */
 package nicon.notify.gui.windowNotify;
 
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import nicon.notify.core.NiconEvent;
+
 /**
- *
+ * Esta clase definirá la creacion de Objetos del tipo WindowNotify en su 
+ * caractiristica de dialogo de confirmacion, por defecto se proponen dos simples
+ * opciones de respuesta si / no, esta clase hereda directamente de WindowNotify
+ * y ajusta la interfaz a su necesidad.
+ * 
  * @author frederick
  */
-public class ConfirmNotify {
+public class ConfirmNotify extends WindowNotify {
+
+    private JButton yesButton;
+    private JButton noButton;
+    
+    private ImageIcon icon;
+    
+    private int option=-1;
+    
+    public ConfirmNotify(NiconEvent ev) {
+        super(ev);
+        setSize(600, 230);
+        init();
+    }
+
+    private void init() {      
+        icon=new ImageIcon(getClass().getResource("/nicon/notify/gui/Icons/NiconInterrogative.png"));
+        setIconMessage(icon);
+        setForegroundTitle(new java.awt.Color(90, 130, 202));
+        
+        jbAcept.setText("Si");
+        jbAcept.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                option=0;
+            }
+        });
+        moveAceptButton(450, 170);
+        
+        noButton=new JButton("No");
+        noButton.setBounds(320, 170, 120, 30);
+        noButton.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               option=1;
+               dispose();
+            }
+        });
+        addButton(noButton);
+    }
+    
+    /**
+     * Este metodo retorna la opcion que ha seleccionado el usuario al momento
+     * de escogar una opcion propuesta por el desarrollador, en ella si el usuariio
+     * presiona el boton si se retornara el valor 0 en caso de decidir que no se
+     * retornará el valor 1.
+     * 
+     * @return int option
+     */
+    public int getSelectedOption(){        
+        return option;
+    }
     
 }
